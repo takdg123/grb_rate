b813806c2b337d7f6921aee38033ab428155d3c8
Donggeun Tak <takdg123@gmail.com>
2021-07-21 13:44:19 +0200

    latest

--- rateStudy.py
+++ rateStudy.py
@@ -1,21 +1,29 @@
+import os
+import sys
+import math
+import pickle
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 import ROOT
 
 from astropy.io import fits
-import os
-import sys
+from astropy.table import Table
+import astropy.units as u
+
 from scipy.optimize import curve_fit
 from scipy import stats
 from scipy.integrate import quad
+from scipy.interpolate import interp1d
+
 from sklearn.neighbors import KernelDensity
+
 from utils import *
-import math
-from astropy.table import Table
+
 from tqdm.notebook import tqdm, trange
 
-import pickle
+
 
 def get_burst_info(GBMData = "./fermi/GBM.fits", grbType="short", plotting=False):
     GRBinfo = fits.open(GBMData)
@@ -94,15 +102,15 @@ def get_GBM_params(GBMData="./fermi/GBM.fits", grbType="short", **kwargs):
     params = np.asarray(params)
     return params
 
-def get_swift_flux(inputDir = "./swift/", grbType="short", wbins = [15, 16, 17], **kwargs):
+def get_swift_flux(inputDir = "./swift/", grbType="short", **kwargs):
     flx = np.load(inputDir+"Swift_flux_{}.npy".format(grbType))
     return flx
 
-def fit_logNlogS_curve(grbType="short", minFlux = None, factor = 1./0.7, wbins = [15, 16, 17], plotting=False, **kwargs):
+def fit_logNlogS_curve(grbType="short", minFlux = None, factor = 1./0.7, wbins=[18, 19, 20], plotting=False, **kwargs):
     GBMflx = get_GBM_flux(grbType=grbType, **kwargs)
     Swiftflx = get_swift_flux(grbType=grbType, **kwargs)
 
-    b = np.logspace(-2, 3, 30)
+    b = np.logspace(-3, 3, 30)
     gy, gx = np.histogram(GBMflx, bins=b)
     gx = center_pt(gx)
     gy = np.cumsum(gy[::-1])[::-1]
@@ -120,7 +128,8 @@ def fit_logNlogS_curve(grbType="short", minFlux = None, factor = 1./0.7, wbins =
     
     if minFlux is None:
         minFlux = min(Swiftflx)
-    N = int(10**p0[0]*minFlux**(-3/2.)/0.7)
+        
+    N = int(10**p0[0]*minFlux**(-3/2.)*factor)
 
     if plotting:
         plt.hist(GBMflx, bins=b, cumulative=-1, histtype='step', label="GBM GRBs")
@@ -129,7 +138,8 @@ def fit_logNlogS_curve(grbType="short", minFlux = None, factor = 1./0.7, wbins =
         
         plt.plot(gx, 10**p0[0]*gx**(-3/2.), label="Linear-fit")
         plt.plot(gx, 10**p0[0]*gx**(-3/2.)*factor, label="Linear-fit (All sky, x{:.2f})".format(factor))
-
+        plt.axvline(b[wbins[0]], ls=":")
+        plt.axvline(b[wbins[-1]+1], ls=":")
         plt.ylim(0.8,10000)
         plt.xscale("log")
         plt.yscale("log")
@@ -158,7 +168,7 @@ def generate_params(N, GBMData="./fermi/GBM.fits", grbType="short"):
     return params
 
 def generate_bursts(logNlogS=None, grbType="short", plotting=False, verbose=True, **kwargs):
-    if logNlogS == None:
+    if logNlogS is None:
         logNlogS = fit_logNlogS_curve(grbType=grbType, **kwargs)
 
     p0 = logNlogS["N_0"]
@@ -200,7 +210,7 @@ def generate_bursts(logNlogS=None, grbType="short", plotting=False, verbose=True
         gbm_params = get_GBM_params(grbType=grbType, **kwargs)
 
         f, ax = plt.subplots(1, 3 if grbType == "short" else 4, figsize=(15, 4))
-        b = np.logspace(-2, 3, 40)
+        b = np.logspace(-3, 3, 40)
         ax[0].hist(GBMflx, bins=b, cumulative=-1, histtype='step', label="GBM GRBs")
         sy, sx, etc = ax[0].hist(Swiftflx, bins=b, cumulative=-1, histtype='step', label="Swift GRBs")
         sx = center_pt(sx)
@@ -239,11 +249,12 @@ def generate_bursts(logNlogS=None, grbType="short", plotting=False, verbose=True
     
     return table
 
-def effective_area(file, plotting = False):
+def effective_area(file, plotting = False, **kwargs):
 
     eff = np.genfromtxt(file)
     eff = eff[np.isfinite(eff[:,1])]
-    ave_eff = np.asarray([eff[:,0][:-1], eff[:,0][1:], center_pt(eff[:,1])]).T
+    temp = center_pt(eff[:,1])
+    ave_eff = np.asarray([eff[:,0][:-1][temp>0], eff[:,0][1:][temp>0], temp[temp>0]]).T
         
     if plotting:
         label = file.split("/")
@@ -256,11 +267,11 @@ def effective_area(file, plotting = False):
         
     return ave_eff
 
-def bkg_from_ROOT(file, eLowEdges, eHighEdges, evttype = "UC", plotting = False):
+def bkg_from_ROOT(file, eLowEdges, eHighEdges, evttype = "UC", plotting = False, **kwargs):
     
     f = ROOT.TFile(file)
     if evttype not in ["UC", "TC", "P"]:
-        print("Error in 'evttype'.")
+        print("[Error] The entered event type is wrong. options: 'evttype' = 'UC', 'TC', or 'P'.")
         return
 
     bkg = []
@@ -268,7 +279,7 @@ def bkg_from_ROOT(file, eLowEdges, eHighEdges, evttype = "UC", plotting = False)
     for l, h in zip(eLowEdges, eHighEdges):
         li = tree.FindBin(l*1000)
         hi = tree.FindBin(h*1000)
-        bkg.append(f.totalBG_UC.Integral(li, hi, "width"))
+        bkg.append(tree.Integral(li, hi, "width"))
     bkg = np.asarray(bkg)
     
     if plotting:
@@ -290,27 +301,80 @@ def bkg_from_ROOT(file, eLowEdges, eHighEdges, evttype = "UC", plotting = False)
     else:
         return bkg
 
-def make_table(eff, bkg):
+def make_table(eff, bkg, **kwargs):
     if type(eff) == str:
-        eff = effective_area(eff)
+        eff = effective_area(eff, **kwargs)
     if type(bkg) == str:
-        bkg = bkg_from_ROOT(bkg, eff[:,0], eff[:,1])
+        bkg = bkg_from_ROOT(bkg, eff[:,0], eff[:,1], **kwargs)
+    table = Table(np.vstack((eff.T, bkg)).T,  names = ("E_low", "E_high", "EA", "background"))
+    table["E_low"].unit = u.MeV
+    table["E_high"].unit = u.MeV
+    table["EA"].unit = u.cm**2
+    table["background"].unit = u.ph/u.s
+    return table
 
-    return Table(np.vstack((eff.T, bkg)).T, names = ("E_low", "E_high", "EA", "background"))
+def dict_to_table(input_dict):
+    rows = []
+    for key in input_dict.keys():
+        if np.size(input_dict[key].value) == 1:
+            rows.append([input_dict[key].value])
+        else:
+            rows.append(input_dict[key].value)
+    table = Table(rows, names = input_dict.keys())
+    for key in input_dict.keys():
+        table[key].unit = input_dict[key].unit
+    return table
+
+
+def plotRateHist(rate, ax = None, label = None, **kwargs):
+
+    if ax is None:
+        ax = plt.gca()
+
+    if label is None:
+        label = "{:.1f} +/- {:.1f}".format(np.average(rate), np.std(rate))
+
+    y, x, etc = ax.hist(rate, histtype="step", label=label)
+    x = center_pt(x)
+    p, cov = curve_fit(gaus, x, y, p0=(20, np.average(rate), 4))
+    fitx = np.linspace(min(rate)/2, max(rate)*2, 100)
+    ax.plot(fitx, gaus(fitx, *p), color=etc[0].get_edgecolor(), ls=":")
+    ax.set_xlabel("GRB rate (GRBs/yr)", fontsize=12)
+    ax.set_ylabel("Counts", fontsize=12)
+    ax.axvline(p[1], color='k')
+    ax.axvline(np.percentile(rate, 16), color=etc[0].get_edgecolor(), ls='--')
+    ax.axvline(np.percentile(rate, 84), color=etc[0].get_edgecolor(), ls='--')
+    ax.legend()
+
+
+def plotFluxHist(output, ax=None, **kwargs):
+    if ax is None:
+        ax = plt.gca()
+
+    y, x = np.histogram(output["flux"], bins=np.logspace(-3, 3, 100))
+    rate = np.cumsum(y[::-1])[::-1]*output["FoV"]
+    plt.step(center_pt(x), rate, where="mid", label="All")
+    y, x = np.histogram(output["flux"][output["snr"]>6.5], bins=np.logspace(-3, 3, 100))
+    rate = np.cumsum(y[::-1])[::-1]*output["FoV"]
+    plt.step(center_pt(x), rate, where="mid", label="Detectable ({} GRBs/yr)".format(int(max(rate))))
+    plt.xscale("log")
+    plt.yscale("log")
+    dist_line = interp1d(rate, center_pt(x), kind='linear')
+    cont90 = dist_line(max(rate)*0.9)
+    plt.axvline(cont90, ls=":", color="k", label = r"90% cont. ({:.1f} ph/cm$^2$/s) ".format(cont90))
+    plt.xlabel(r"Flux ({:.1f} - {:.1f} MeV) [ph/cm$^2$/s]".format(output["eRange"][0]/1000., output["eRange"][1]/1000.), fontsize=12)
+    plt.ylabel("Rate [GRBs/yr]", fontsize=12)
+    plt.legend()
 
 class EstimateRate:
 
-    def __init__(self, inst=None, grbType="short", snr_cut = 6.5, FoV = None, load_status = False, verbose=False):
+    def __init__(self, inst=None, grbType="short", snr_cut = 6.5, FoV = None, load_status = False, verbose=False, **kwargs):
 
         if load_status:
-            filename = "{}.pickle".format(load_status)
+            filename = "./output/{}.pickle".format(load_status)
             with open(filename, 'rb') as file:
                 self.__dict__.update(pickle.load(file).__dict__)
         else:
-            if type(inst) != Table:
-                print("[Error] The type of input is wrong (require astropy.table).")
-                return
-
             self._grbType = grbType
             self._inst = inst
             
@@ -319,6 +383,7 @@ class EstimateRate:
             self._FoV = obs_factor = 1./10.*FoV
             self._verbose = verbose
             self._snr_cut = snr_cut
+            self._eRange = kwargs.get("eRange", [200, 1000])
 
     def create_spectra(self, **kwargs):
         verbose = kwargs.get("verbose", self.verbose)
@@ -326,9 +391,9 @@ class EstimateRate:
             print("[Log] Generating burst spectra")
         return generate_bursts(grbType=self.grbType, verbose=verbose)
 
-    def forward_folding(self, burst_table=None, inst_table=None, calc_flux = True, return_output=False, **kwargs):
+    def forward_folding(self, burst_table=None, inst_table=None, calc_flux = True, return_output=False, save_result=False, filename=None, **kwargs):
         verbose = kwargs.get("verbose", self.verbose)
-        eRange = kwargs.get("eRange", [200, 1000])
+        self._eRange = kwargs.get("eRange", self.eRange)
 
         if burst_table is None:
             self.burst_table = self.create_spectra(verbose=verbose)
@@ -336,16 +401,19 @@ class EstimateRate:
             self.burst_table = burst_table
 
         if inst_table is None:
-            background = self.inst["background"]
-            inst_table = self.inst
-        elif type(inst_table) == dict:
-            background = [inst_table["background"]]
-            inst_table = [inst_table]
-
-
+            if self.inst is None:
+                print("[Error] The EA and background cannot be loaded.")
+                return
+            else:
+                inst_table = self.inst
+            
+        conv_factor = [(inst_table["E_low"].unit).to(u.keV),
+                       (inst_table["E_high"].unit).to(u.keV),
+                       (inst_table["EA"].unit).to(u.cm**2)]
+            
         cnts_tot = []
         flx_tot = []
-        
+
         if verbose:
             print("[Log] Forward folding the burst spectra")
         for burst in tqdm(self.burst_table) if verbose else self.burst_table:
@@ -354,36 +422,52 @@ class EstimateRate:
             alpha = burst["alpha"]
             ep = 10**burst["log10(Ep)"]
             for channel in inst_table:
-                elow = channel["E_low"]
-                ehigh = channel["E_high"]
-                eff = channel["EA"]
+                elow = channel["E_low"]*conv_factor[0]
+                ehigh = channel["E_high"]*conv_factor[1]
+                eff = channel["EA"]*conv_factor[2]
                 if self.grbType == "short":
-                    cnts_per_cm2 = quad(CUTOFFPL, elow*1000., ehigh*1000., args=(norm, alpha, ep))[0]
+                    cnts_per_cm2 = quad(CUTOFFPL, elow, ehigh, args=(norm, alpha, ep))[0]
                 elif self.grbType == "long":
                     beta = burst["beta"]
-                    cnts_per_cm2 = quad(BAND, elow*1000., ehigh*1000., args=(norm, alpha, ep, beta))[0]
+                    cnts_per_cm2 = quad(BAND, elow, ehigh, args=(norm, alpha, ep, beta))[0]
                 cnts.append(cnts_per_cm2*eff)
             cnts_tot.append(cnts)
             
             if calc_flux:
                 if self.grbType == "short":
-                    flx_tot.append(quad(CUTOFFPL, eRange[0], eRange[1], args=(norm, alpha, ep))[0])
+                    flx_tot.append(quad(CUTOFFPL, self.eRange[0], self.eRange[1], args=(norm, alpha, ep))[0])
                 elif self.grbType == "long":
-                    flx_tot.append(quad(BAND, eRange[0], eRange[1], args=(norm, alpha, ep, beta))[0])
+                    flx_tot.append(quad(BAND, self.eRange[0], self.eRange[1], args=(norm, alpha, ep, beta))[0])
 
         self.counts = np.asarray(cnts_tot)
-        self.background = background
+        self.background = inst_table["background"]
         self.flux = np.asarray(flx_tot)
         self.snr = self.calc_snr(self.counts, self.background)
         self.rate = self.calc_rate(self.snr, **kwargs)
-        
+        self.output = {"counts": self.counts, 
+                       "background": self.background, 
+                       "flux": self.flux, 
+                       "snr": self.snr, 
+                       "parameters": self.burst_table, 
+                       "rate": self.rate, 
+                       "FoV": self.FoV,
+                       "eRange": self.eRange}
+
         if verbose:
             snr_cut = kwargs.get("snr_cut", self.snr_cut)
-            print("[Log] Analysis done. Estimated rate is {:.1f} GRBs/yr (SNR > {})".format(self.rate, snr_cut))
-        
+            print("[Log] Analysis done. The estimated rate is {:.1f} GRBs/yr (SNR > {})".format(self.rate, snr_cut))
+
+        if save_result:
+            if filename is None:
+                filename = "current.npy"
+            elif ".npy" not in filename:
+                filename = filename+".npy"
+            filename = "./output/"+filename
+            np.save(filename, self.output)
+            print("[Log] The result is saved in {}".format(filename))
+
         if return_output:
-            output = {"counts": self.counts, "background": self.background, "flux": self.flux, "snr": self.snr, "parameters": self.burst_table, "rate": self.rate}
-            return output
+            return self.output
 
     def calc_rate(self, snr, **kwargs):
         snr_cut = kwargs.get("snr_cut", self.snr_cut)
@@ -394,46 +478,30 @@ class EstimateRate:
             return sum((snr>snr_cut).T)*FoV
 
     def calc_snr(self, cnts, bkg):
-        return np.asarray([max(s) for s in cnts/np.sqrt(cnts+bkg)])
+        return np.asarray([max(s) for s in np.nan_to_num(cnts/np.sqrt(cnts+bkg))])
 
     def simulation(self, save_status="current", runs=100, inst_table=None, **kwargs):
-        snr_sim = []
+        if inst_table is None:
+            if self.inst is None:
+                print("[Error] The EA and background cannot be loaded.")
+                return
+            else:
+                inst_table = self.inst
+
+        output = []
         rate_sim = []
-        flux_sim = []
         for i in trange(runs):
             out = self.forward_folding(inst_table=inst_table, return_output=True, **kwargs)
-            snr_sim.append(out["snr"])
+            output.append(out)
             rate_sim.append(out["rate"])
-            flux_sim.append(out["flux"])
 
-        self.snr_sim = np.asarray(snr_sim)
+        self.output_sim = np.asarray(output)
         self.rate_sim = np.asarray(rate_sim)
-        self.flux_sim = np.asarray(flux_sim)
-        filename = "{}.pickle".format(save_status)
+
+        filename = "./output/{}.pickle".format(save_status)
         with open(filename, 'wb') as file:
             pickle.dump(self, file)
-        print("[Log] Analysis Done (status: '{}'). The rate is {:.1f} +/- {:.1f} GRBs/yr".format(save_status, np.average(self.rate_sim), np.std(self.rate_sim)))
-
-    @classmethod
-    def plotRateHist(self, rate, label = None, ax = None):
-
-        if ax is None:
-            ax = plt.gca()
-
-        if label is None:
-            label = "{:.1f} +/- {:.1f}".format(np.average(rate), np.std(rate))
-        
-        y, x, etc = ax.hist(rate, histtype="step", label=label)
-        x = center_pt(x)
-        p, cov = curve_fit(gaus, x, y, p0=(20, np.average(rate), 4))
-        fitx = np.linspace(min(rate)/2, max(rate)*2, 100)
-        ax.plot(fitx, gaus(fitx, *p), color=etc[0].get_edgecolor(), ls=":")
-        ax.set_xlabel("GRB rate (GRBs/yr)", fontsize=12)
-        ax.set_ylabel("Counts", fontsize=12)
-        ax.axvline(p[1], color='k')
-        ax.axvline(np.percentile(rate, 16), color=etc[0].get_edgecolor(), ls='--')
-        ax.axvline(np.percentile(rate, 84), color=etc[0].get_edgecolor(), ls='--')
-        ax.legend()
+        print("[Log] Analysis Done (status: '{}'). The estimated rate is {:.1f} +/- {:.1f} GRBs/yr".format(save_status, np.average(self.rate_sim), np.std(self.rate_sim)))
 
     @property
     def grbType(self):
@@ -450,6 +518,10 @@ class EstimateRate:
     @property
     def snr_cut(self):
         return self._snr_cut
+
+    @property
+    def eRange(self):
+        return self._eRange
 
     @property
     def verbose(self):
